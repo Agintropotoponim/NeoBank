41575ff8184d0ac3f91bc16356383d173979220b
Agintropotoponim <karaspinf@gmail.com>
2024-11-26 19:02:59 +0300

    feat: add blue-button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,13 @@ import './App.scss';
 import '../shared/styles/globals.css'
 import { NavBar } from '../entities/NavBar';
 import { BrowserRouter } from 'react-router-dom';
+import { BlueButton } from '../shared/ui/BlueButton/BlueButton';
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <NavBar />
+                <BlueButton className='home-page__button_size'>Online Bank</BlueButton>
             </BrowserRouter>
         </div>
     );
