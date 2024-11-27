f479dce90353a9524abb284e03b4a7186e6c3136
Agintropotoponim <karaspinf@gmail.com>
2024-11-28 02:28:44 +0300

    feat: add hr and footer

---
+++ src/shared/consts/contentFooterLinks.ts
@@ -0,0 +1,14 @@
+import { IContentFooterLink } from "../types/contentNavTypes";
+
+export const CONTENT_FOOTER_LINKS: IContentFooterLink[] = [
+    { title: 'About bank', link: '' },
+    { title: 'Ask a Question', link: '' },
+    { title: 'Quality of service', link: '' },
+    { title: 'Requisites', link: '' },
+    { title: 'Press center', link: '' },
+    { title: 'Bank career', link: '' },
+    { title: 'Investors', link: '' },
+    { title: 'Analytics', link: '' },
+    { title: 'Business and processes', link: '' },
+    { title: 'Compliance and business ethics', link: '' },
+]
