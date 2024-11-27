6e722bd1c67589ad1198c2fd03f8de1e2a3c7622
Agintropotoponim <karaspinf@gmail.com>
2024-11-27 16:37:34 +0300

    style: fix some adaptability issues

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,3 +1,4 @@
+import { ChooseCard } from '../../../entities/ChooseCard'
 import { Header } from '../../../widgets/Header'
 import classes from './HomePage.module.scss'
 
@@ -5,6 +6,7 @@ export const HomePage: React.FC = () => {
     return (
         <div className={classes['home-page']}>
             <Header />
+            <ChooseCard />
         </div>
     )
 }
